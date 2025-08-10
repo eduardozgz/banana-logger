86540d70e756dd1aff7c9ef7fbed0d60440e34af
eduardozgz <eduardozgz@protonmail.com>
2025-08-10 20:53:09 +0200

    dont prettify prisma

--- packages/db/eslint.config.js
+++ packages/db/eslint.config.js
@@ -3,7 +3,7 @@ import baseConfig from "@/eslint-config/base";
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
-    ignores: [],
+    ignores: ["prisma/generated/**/*"],
   },
   ...baseConfig,
 ];
