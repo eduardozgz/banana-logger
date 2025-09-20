5927de2a5608fc9dcd2ae10031bba5f6339b7ae8
eduardozgz <eduardozgz@protonmail.com>
2025-09-20 21:44:31 +0200

    refactor

--- apps/img/src/main.rs
+++ apps/img/src/main.rs
@@ -4,14 +4,15 @@ mod generators;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let address = "0.0.0.0";
+    let port = std::env::var("IMG_PORT")
+        .unwrap_or("8080".to_string())
+        .parse()
+        .unwrap();
+
+    println!("Starting server on port {}", port);
     HttpServer::new(|| App::new().service(generators::color::index))
-        .bind((
-            "0.0.0.0",
-            std::env::var("IMG_PORT")
-                .unwrap_or("8080".to_string())
-                .parse()
-                .unwrap(),
-        ))?
+        .bind((address, port))?
         .run()
         .await
 }
